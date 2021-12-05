1fad30fcd4cf8a32fbc7f5ba7ac4900b6ee6722b
Tobias Kuhn <tobias.k.99@web.de>
2021-12-05 16:06:36 +0100

    idk

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,14 +5,19 @@ import java.awt.event.ActionListener;
 public class HelloWorld {
     private JButton halloButton;
     private JPanel mainPanel;
+    private JTextField nameField;
+    private JLabel nameLabel;
 
     public HelloWorld() {
         halloButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(halloButton ,"Herzlich Willkommen!");
+                String name = nameField.getText();
+                System.out.println("Hallo " +name+"!");
             }
         });
+
     }
 
     public static void main(String[] args) {
