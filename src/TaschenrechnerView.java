499ef033c97691ab839a2e744b0de4177c6144f0
Tobias Kuhn <tobias.k.99@web.de>
2021-12-06 17:56:17 +0100

    6.1

---
+++ src/TaschenrechnerView.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class TaschenrechnerView {
+    private JTextField anzeige;
+    private JButton vierButton;
+    private JButton einsButton;
+    private JButton siebenButton;
+    private JButton zweiButton;
+    private JButton fünfButton;
+    private JButton achtButton;
+    private JButton dreiButton;
+    private JButton sechsButton;
+    private JButton neunButton;
+    private JButton nullButton;
+}
